f65f6529a747dcace78baddf1c40fd7a9de8108f
Pedro Paiva <pedrovsp109@gmail.com>
2019-04-09 21:11:29 -0300

    Extrat Timeline and Dashboard classes
    Using Single Responsability Principle

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -3,8 +3,6 @@ class Shout < ApplicationRecord
   belongs_to :content, polymorphic: true
 
   validates :user, presence: true
-  
-  default_scope { order(created_at: :desc) }
 
   delegate :username, to: :user
 end
